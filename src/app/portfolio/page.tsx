3eda0be70eb6c4ff69088b43332a8e5bd30e3582
shuremali02 <shuremsyed41@gmail.com>
2024-10-01 14:12:30 -0700

    portfoliopost

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -35,11 +35,27 @@ export default function Portfolio() {
       route: "https://next-js-milestone-1-achieved.netlify.app/",
     },
     {
-      img: "/Capture.PNG",
-      title: "Static Resume",
-      description: "Static Resume use for html,css and typescript",
-      route: "https://hackathon-static-resume-builder.netlify.app",
+      img: "/image.png",
+      title: "Dynamic Resume Builder",
+      description: "Dynamic Resume Builder use to Generate your Resume and save as PDF ",
+      route: "https://resume-builder-milestone-5.netlify.app/",
+    
     },
+    {
+      img: "/BlogPost.jpg",
+      title: "Blogpost",
+      description: "This is My BlogPost-web ",
+      route: "https://blogpost101.netlify.app/",
+    
+    },
+    {
+      img: "/travelBlog.jpg",
+      title: "Travelling-Website- ",
+      description: "A modern travel blog built with Next.js, React, and Tailwind CSS, The site features responsive design, dynamic components, and smooth navigation for an engaging travel journey.",
+      route: "https://travelblog-assignment.netlify.app/",
+    
+    },
+
 
 
   ];
